4e40e8d270c6c7da5c9d56643de690dca1008f5c
Akshay <47742322+SalientAuthor@users.noreply.github.com>
2023-04-18 00:31:43 +0530

    files

---
+++ SReact/Blog.js
@@ -0,0 +1,179 @@
+import logo from "./logo.svg";
+import "./Blog.css";
+
+function Blog() {
+  return (
+    <div className="blank">
+      <nav className="navBar">
+        <div className="leftBar"></div>
+        <div className="rightBar">
+          <button className="navBtn">Blog</button>
+          <button className="navBtn" id="signUp">
+            Sign up
+          </button>
+          <button className="navBtn">Login</button>
+        </div>
+      </nav>
+      <div className="blogBody">
+        <section>
+          <div className="blogCover">
+            <p className="blogTitle">ATTACK ON TITANS</p>
+            <p className="rating">★★★★★</p>
+            <p className="reads"> 1 million + reads</p>
+          </div>
+          <div className="chapter">
+            <h1>CHAPTER 1</h1>
+            <hr />
+            <article>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
+              alias, similique deserunt rerum dicta a omnis voluptatem velit
+              consequatur corrupti quas dolores, nisi quo aspernatur magni
+              aliquam quidem vero dolore. Hic ex blanditiis quod, omnis
+              voluptate, accusamus aliquid, nihil quo recusandae dicta sapiente.
+              Omnis modi ipsum qui mollitia nostrum quo accusantium deserunt
+              veniam ex officia commodi cupiditate doloribus laborum, sapiente
+              quisquam ratione sed quis eligendi eaque eos quae. Voluptas minima
+              quibusdam mollitia veritatis nulla tempore voluptatem ex harum,
+              eos eligendi expedita quidem totam exercitationem voluptate, iure
+              vero modi eveniet reiciendis doloribus fugit earum. Itaque
+              repellat minus magni consequuntur, maxime ut odio rem nemo ad
+              accusantium soluta fugit? Ipsum quibusdam quae quod nesciunt,
+              suscipit vero minus, fugit, aliquam hic sed reprehenderit non. Ut
+              minus necessitatibus quam maxime reprehenderit enim. Consequatur
+              ut temporibus dolor numquam accusantium aspernatur excepturi nihil
+              obcaecati et reiciendis, id dicta molestiae. Hic, perspiciatis
+              laudantium exercitationem, cupiditate, rerum amet ipsam illo quo
+              nostrum eius deleniti corporis laborum architecto sint magnam
+              minima adipisci! Autem architecto reprehenderit recusandae tenetur
+              quam laboriosam aut possimus iste, illum debitis repudiandae
+              cupiditate ratione doloribus quidem? Soluta rem dolorem earum
+              culpa officia voluptates enim aliquid at blanditiis? Ipsa
+              voluptate velit voluptatem corrupti harum modi molestiae ratione
+              magni ipsam corporis non magnam, sunt placeat laboriosam dolor
+              eius atque fuga! Beatae libero nesciunt quos sapiente inventore
+              error sit aperiam natus magni voluptas? Animi earum quae veritatis
+              a ipsa facere aut deleniti, magnam quasi reprehenderit labore
+              explicabo veniam. Ad molestias eligendi aut veritatis quia
+              voluptatum, eveniet itaque. Eos iusto voluptatum minus ipsam in,
+              incidunt id. Sit modi, omnis consequatur iure est quae animi
+              nesciunt unde sapiente officiis nam veniam repudiandae corporis?
+              Molestias in fugiat quos delectus repellendus accusamus dolor
+              labore autem dolorum ratione hic natus quis commodi, ea sequi
+              deleniti, nesciunt dolores itaque quam. Quibusdam autem placeat,
+              expedita praesentium quo quis iure quam pariatur nihil possimus
+              maiores culpa. Iure commodi veniam natus fugit! In fuga laudantium
+              totam dolores quam. Nisi sequi consequatur mollitia veniam animi,
+              natus adipisci eius soluta recusandae, beatae quisquam labore
+              magnam illum omnis. Illo harum quasi et inventore cum corrupti
+              debitis quod accusantium, saepe quidem ipsa, itaque vitae laborum
+              provident molestiae maxime reiciendis voluptas, unde ratione
+              similique doloremque soluta voluptatum fugit rerum. Magni
+              praesentium minima obcaecati, exercitationem rem blanditiis porro
+              dignissimos et repudiandae odio voluptates aut voluptate. Aliquam
+              quo voluptas fuga similique earum nemo ut modi aperiam, qui,
+              ratione temporibus suscipit quisquam deleniti eligendi pariatur
+              aliquid dolores soluta molestiae esse ea laborum incidunt quidem
+              doloremque minus! Consectetur aspernatur ipsam suscipit dolores
+              veritatis omnis quibusdam mollitia magni minus reprehenderit optio
+              assumenda, ipsum, maxime, in repudiandae dicta dignissimos
+              similique. Iste delectus corrupti odio ex deleniti accusamus amet
+              reiciendis ut ipsa totam autem ea tempore accusantium nostrum
+              aliquam assumenda magnam, labore sit expedita aut cupiditate?
+              Dolores suscipit, at ea rerum similique cum enim aperiam
+              laudantium quis! Inventore in facilis voluptate, animi laborum,
+              veritatis ad soluta neque, ab est sapiente deleniti consequuntur?
+              Odit saepe, sint itaque ipsum sed quaerat architecto sit cumque
+              explicabo dolor nesciunt minima ut dicta reiciendis ea. Magnam
+              dolor nesciunt officiis eum unde explicabo exercitationem soluta
+              deserunt beatae optio nobis fugit quibusdam, aliquid commodi porro
+              debitis alias maiores sed aperiam quidem cumque veniam earum
+              maxime hic. Ipsum voluptatibus sunt iure, quod nesciunt,
+              necessitatibus iusto nulla alias suscipit amet numquam, pariatur
+              beatae corporis animi maiores omnis quidem exercitationem rerum
+              tempora quia asperiores commodi quisquam cupiditate? Rem sed eum
+              minima recusandae unde nisi consequuntur, iste voluptate facere
+              non cupiditate inventore ullam expedita ipsa quod eaque harum
+              quibusdam alias neque perspiciatis, sequi provident odio deserunt!
+              Accusantium nobis saepe, quibusdam dicta id voluptate aliquam
+              minus consequatur dolorum aspernatur, sint cumque! Quaerat
+              asperiores laboriosam in obcaecati animi quod id ex similique
+              dignissimos accusantium nulla, cupiditate minima tenetur molestiae
+              deserunt odit quis dolore. At perspiciatis inventore explicabo
+              quaerat beatae commodi error quisquam nesciunt nisi exercitationem
+              autem eos eius vero dolore nihil ipsa ex aperiam perferendis, quas
+              maxime nemo, amet expedita magni? Cupiditate nulla cum non illum
+              ipsam quam consequatur consectetur ducimus aliquid totam!
+              Obcaecati natus dignissimos similique, distinctio deleniti iste
+              odit reiciendis incidunt odio illo quis labore aliquid, pariatur
+              ipsum nemo possimus. Ducimus, modi id cum sapiente repellat facere
+              impedit, fuga reprehenderit non, libero fugit? Delectus, iure
+              reprehenderit magni voluptatibus incidunt commodi cum. `<br />
+              <br />`
+            </article>
+            <hr />
+            <h1 className="switchChapter">CHAPTER 2</h1>
+          </div>
+        </section>
+        <div className="shortReads">
+          <h3>Short Reads</h3>
+          <hr />
+          <div className="blogs">
+            <div className="item1">
+              <div className="minicover">
+                <img
+                  src="https://nefariousreviews.files.wordpress.com/2017/06/akame-ga-kill-featured.jpg"
+                  alt="no cover"
+                />
+              </div>
+              <div className="info">
+                <div className="title">
+                  <p>Akame Ga Kill: Season finale</p>
+                </div>
+                <div className="desc">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed,
+                  eius.
+                </div>
+              </div>
+            </div>
+            <div className="item2">
+              <div className="minicover">
+                <img
+                  src="https://i.pinimg.com/originals/6f/f4/10/6ff410745726fd2e4464f108974cff98.png"
+                  alt=""
+                />
+              </div>
+              <div className="info">
+                <div className="title">
+                  <p>Naruto Uzumaki: Hidden Village</p>
+                </div>
+                <div className="desc">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Officia, numquam?
+                </div>
+              </div>
+            </div>
+            <div className="item3">
+              <div className="minicover">
+                <img
+                  src="https://c4.wallpaperflare.com/wallpaper/463/852/836/itou-noiji-flyable-heart-inaba-yui-boy-wallpaper-thumb.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="info">
+                <div className="title">
+                  <p>Love juice Season Priemere</p>
+                </div>
+                <div className="desc">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Dicta, necessitatibus!
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
